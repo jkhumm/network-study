1a1d48379813f1983dcff0ad0161d8852754102f
jkhumm <1158139789@qq.com>
2021-02-27 15:00:24 +0800

    humm commit

--- src/main/java/com/dongnaoedu/network/Application.java
+++ src/main/java/com/dongnaoedu/network/Application.java
@@ -1,15 +1,31 @@
 package com.dongnaoedu.network;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
+@Slf4j
 // @EnableAsync // springboot全局异步，在这里加此注解，但是没有限制不建议这样用
 public class Application {
 
-    public static void main(String[] args) {
-        System.out.println(111);
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ApplicationContext application = SpringApplication.run(Application.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "Swagger-UI: http://" + ip + ":" + port + path + "/doc.html\n" +
+                "----------------------------------------------------------");
     }
 
 }

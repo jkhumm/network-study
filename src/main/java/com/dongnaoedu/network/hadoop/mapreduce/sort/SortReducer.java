8d13a2fad638ab694c85aaf1ce47830ea416f363
jkhumm <1158139789@qq.com>
2022-03-24 11:42:16 +0800

    自定义计数器

--- src/main/java/com/dongnaoedu/network/hadoop/mapreduce/sort/SortReducer.java
+++ src/main/java/com/dongnaoedu/network/hadoop/mapreduce/sort/SortReducer.java
@@ -7,7 +7,14 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
 public class SortReducer extends Reducer<PairWritable, Text,PairWritable, NullWritable> {
-  
+
+
+    // 自定义计数器使用枚举方式创建
+    public static enum MyCounter{
+        REDUCE_INPUT_RECORDS,
+        RECUDE_INPUT_VAL_NUMS
+    }
+
     @Override
     protected void reduce(PairWritable key,Iterable<Text> values,Context context) throws IOException,InterruptedException{
         // 我们仅仅是将v2的值传给v3，然后定义v3为个占位符空对象 
@@ -18,8 +25,12 @@ public class SortReducer extends Reducer<PairWritable, Text,PairWritable, NullWr
          * ==>  a 1 <a 1,a 1>
          */
 
+        // 统计key个数
+        context.getCounter(MyCounter.REDUCE_INPUT_RECORDS).increment(1L);
+
         for(Text value:values){
             // 为了防止导致重复结果不输出，把每个结果作为key都输出出来
+            context.getCounter(MyCounter.RECUDE_INPUT_VAL_NUMS).increment(1L);
             context.write(key, NullWritable.get());
         }
     }
--- src/main/java/com/dongnaoedu/network/hadoop/mapreduce/sort/WordCountMapper.java
+++ src/main/java/com/dongnaoedu/network/hadoop/mapreduce/sort/WordCountMapper.java
@@ -2,6 +2,7 @@ package com.dongnaoedu.network.hadoop.mapreduce.sort;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
@@ -9,12 +10,18 @@ import java.io.IOException;
 public class WordCountMapper extends Mapper<LongWritable,Text,PairWritable,Text>{
     @Override
     public void map(LongWritable key, Text value, Mapper.Context context) throws IOException,InterruptedException{
+
+        // 自定义计数器  第一个参数计数器的分类，第二个参数计时器分类的其中一个计数器
+        Counter counter = context.getCounter("MR_COUNT", "MapRecueCounter");
+        counter.increment(1L);
+
         // 对每一行数据进行拆分，然后封装到PairWritable对象中作为k2
         String[] split =  value.toString().split("\t");
         PairWritable obj = new PairWritable();
         obj.setFirst(split[0]);
         obj.setSecond(Integer.parseInt(split[1]));
-        
+
+
         //将k2和v2传递给context 
         context.write(obj,value);
     }
